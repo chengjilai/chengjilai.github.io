24e96c347be28d2ca030eceb7d92fc6f047de2af
chengjilai <chengjilai@sjtu.edu.cn>
2026-02-06 02:25:29 +0800

    feat: References list on every post page (exact-config-keys)

--- posts/exact-config-keys.js
+++ posts/exact-config-keys.js
@@ -80,6 +80,8 @@ document.body.appendChild($("p", {}, [
   "systemd builtins post.",
 ]));
 
+appendReferences();
+
 document.body.appendChild($("p", {}, [
   "Source: ",
   $("a", { href: "https://github.com/chengjilai/chengjilai.github.io", textContent: "github.com/chengjilai/chengjilai.github.io" }),

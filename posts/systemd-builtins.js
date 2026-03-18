b36753dd19ae7078ed86f1d0d84acbd11ff6536e
chengjilai <chengjilai@sjtu.edu.cn>
2026-03-19 02:25:29 +0800

    feat: References list on every post page (systemd-builtins)

--- posts/systemd-builtins.js
+++ posts/systemd-builtins.js
@@ -131,6 +131,8 @@ document.body.appendChild($("p", {}, [
   ".",
 ]));
 
+appendReferences();
+
 document.body.appendChild($("p", {}, [
   "Source: ",
   $("a", { href: "https://github.com/chengjilai/chengjilai.github.io", textContent: "github.com/chengjilai/chengjilai.github.io" }),

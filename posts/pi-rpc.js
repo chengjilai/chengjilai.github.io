eb2168bed8a706dc86040b89ac763452c72cac16
chengjilai <chengjilai@sjtu.edu.cn>
2026-07-15 02:25:29 +0800

    feat: References list on every post page (pi-rpc)

--- posts/pi-rpc.js
+++ posts/pi-rpc.js
@@ -90,6 +90,8 @@ document.body.appendChild($("p", {}, [
 ]));
 
 
+appendReferences();
+
 document.body.appendChild($("p", {}, [
   "Source: ",
   $("a", { href: "https://github.com/chengjilai/chengjilai.github.io", textContent: "github.com/chengjilai/chengjilai.github.io" }),
